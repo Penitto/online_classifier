b152a976d8c8131c1de5ea5796811b980270dd0b
Penitto <penitto02@gmail.com>
2022-04-16 14:24:54 +0300

    It works but predict some shit

--- web/app.py
+++ web/app.py
@@ -10,35 +10,33 @@ from torchvision.transforms import ToTensor
 import cv2
 import numpy as np
 
-# Настройка Flask
-app = Flask(__name__, template_folder=os.environ['FLASK_TEMPLATES'])
+# Flask
+app = Flask(__name__, 
+   template_folder=os.environ['FLASK_TEMPLATES'], 
+   static_folder=os.environ['FLASK_STATIC'])
 app.secret_key = os.environ['FLASK_SECRET_KEY']
-# app.config['UPLOAD_FOLDER'] = FLASK_UPLOAD_FOLDER
-
-
-# Настройка Mongo
 app.config['MONGO_URI'] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ":" + os.environ['MONGODB_PASSWORD'] + '@' + \
    os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
+# Mongo
 client = MongoClient(app.config['MONGO_URI'])
 db = client[os.environ['MONGODB_DATABASE']]
 fs = gridfs.GridFS(db)
 
-# Создание папки загрузки, если на данный момент таковой нет
-# if not os.path.exists(FLASK_UPLOAD_FOLDER):
-#    os.mkdir(FLASK_UPLOAD_FOLDER)
-
-# Выгрузка модели
+# Модель
 model = mobilenet_v3_small(pretrained=False, progress=False)
 model.state_dict = torch.load(os.environ['MODEL_PATH'])
 model.eval()
 
-# Загрузили классы, на которые мы предсказываем
+# Классы
 img_class_map = None
 mapping_file_path = os.environ['MODEL_CLASSES_FILE']
 if os.path.isfile(mapping_file_path):
     with open (mapping_file_path) as f:
         img_class_map = json.load(f)
+
+if not os.path.exists(os.environ['FLASK_STATIC']):
+   os.mkdir(os.environ['FLASK_STATIC'])
 
 # Проверка на разрешение файла
 def allowed_file(filename):
@@ -48,6 +46,15 @@ def allowed_file(filename):
 # Главная страница
 @app.route('/', methods=['GET', 'POST'])
 def main():
+
+   static_files = os.listdir(os.environ['FLASK_STATIC'])
+
+   if static_files:
+      for file in static_files:
+         try:  
+            os.remove(os.environ['FLASK_STATIC'] + '/' + file)
+         except:
+            print('Unable to delete because of lapki')
    return render_template('start.html')
 
 # Страница с результатом
@@ -81,11 +88,13 @@ def upload_file():
          image = np.fromstring(image, np.uint8)
          image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
          image = cv2.resize(image, (int(os.environ['MODEL_IMAGE_HEIGHT']), int(os.environ['MODEL_IMAGE_WIDTH'])))
+         cv2.imwrite(os.environ['FLASK_STATIC'] + '/' + filename, image)
          image = torch.unsqueeze(ToTensor()(image), 0)
          prediction = torch.argmax(model.forward(image).data).numpy()
 
-         
-         return render_template('result.html', prediction=img_class_map[prediction])
+         return render_template('result.html', 
+            prediction=img_class_map[prediction], 
+            filename=filename)
 
    return 
 
