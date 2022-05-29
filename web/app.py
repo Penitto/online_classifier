613a3da862988d1d7725ac2b81fb916f0ae19696
Penitto <penitto02@gmail.com>
2022-05-29 17:59:48 +0300

    Major changes

--- web/app.py
+++ web/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template, flash, redirect
-from pymongo import MongoClient
+from flask import Flask, request, render_template, redirect
+from flask.logging import default_handler
 from werkzeug.utils import secure_filename
 import os
-import gridfs
 import json
 import torch
+import logging
 from torchvision.models import mobilenet_v3_small
 from torchvision.transforms import ToTensor
 import cv2
@@ -15,13 +15,9 @@ app = Flask(__name__,
    template_folder=os.environ['FLASK_TEMPLATES'], 
    static_folder=os.environ['FLASK_STATIC'])
 app.secret_key = os.environ['FLASK_SECRET_KEY']
-app.config['MONGO_URI'] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ":" + os.environ['MONGODB_PASSWORD'] + '@' + \
-   os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
-# Mongo
-client = MongoClient(app.config['MONGO_URI'])
-db = client[os.environ['MONGODB_DATABASE']]
-fs = gridfs.GridFS(db)
+root = logging.getLogger()
+root.addHandler(default_handler)
 
 # Модель
 model = mobilenet_v3_small(pretrained=False, progress=False)
@@ -49,13 +45,6 @@ def main():
 
    static_files = os.listdir(os.environ['FLASK_STATIC'])
 
-   if static_files:
-      for file in static_files:
-         try:  
-            os.remove(os.environ['FLASK_STATIC'] + '/' + file)
-         except:
-            print('Unable to delete because of lapki')
-
    return render_template('start.html')
 
 # Страница с результатом
@@ -65,31 +54,28 @@ def upload_file():
       
       # Проверили, что файл есть
       if 'file' not in request.files:
-         flash('No file part')
+         app.logger.warning('No file part')
          return redirect('/')
       file = request.files['file']
       
       if file.filename == '':
-         flash('No selected file')
+         app.logger.warning('No selected file')
          return redirect('/')
 
       # Если с файлом всё ок
       if file and allowed_file(file.filename):
-         filename = secure_filename(file.filename)
-         # mongo.save_file(filename, file)
-         # Положили в базу
-         fs.put(file, filename=filename)
 
-         # Логнули об этом
-         app.logger.info('%s inserted in database', filename)
+         # Сохраняем файл
+         filename = secure_filename(file.filename)
+         filename = os.path.join(os.environ['FLASK_STATIC'], filename)
+         file.save(filename)
          
+         # Логнули об этом
+         app.logger.warning('%s saved', filename)
 
          # Выгрузили изображение из базы
-         image = fs.get_last_version(filename).read()
-         image = np.fromstring(image, np.uint8)
-         image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
+         image = cv2.imread(filename)
          image = cv2.resize(image, (int(os.environ['MODEL_IMAGE_HEIGHT']), int(os.environ['MODEL_IMAGE_WIDTH'])))
-         cv2.imwrite(os.environ['FLASK_STATIC'] + '/' + filename, image)
          image = torch.unsqueeze(ToTensor()(image), 0)
          prediction = torch.argmax(model.forward(image).data).numpy()
 
